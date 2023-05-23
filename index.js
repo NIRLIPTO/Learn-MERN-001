a9d5f83a1191de20dc62c2e3878ca521fb715569
NIRLIPTO <39208468+NIRLIPTO@users.noreply.github.com>
2023-05-23 06:19:22 +0600

    callback function

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>LEARN-MERN-001</h1>`;
 
-console.log("SPEECH-SAVER")
+console.log('SPEECH-SAVER');
+
+const syncWithCallBack = () => {
+  for (let i = 1; i < 4; i++) {
+    console.log('index ', i, '\t', new Date());
+  }
+};
+
+syncWithCallBack();
+console.log("Completed")
